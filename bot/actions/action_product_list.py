7954ca2db4990e00190f6e7f066ff5a115b7897b
Maíra Bello <maira.bello@vtex.com.br>
2021-05-03 15:22:19 -0300

    Print final extracted products list

--- bot/actions/action_product_list.py
+++ bot/actions/action_product_list.py
@@ -10,7 +10,15 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 
+from pprint import pformat
+
 import os
+
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level="DEBUG")
+
 
 class ActionProductList(Action):
     def name(self) -> Text:
@@ -25,12 +33,25 @@ class ActionProductList(Action):
         # TODO: query VTEX's product list database to match the products
         # with what the user has ordered
 
-        vtex_store = os.getenv('VTEX_STORE')
-        vtex_store_api_key = os.environ.get('VTEX_STORE_API_KEY')
-        vtex_store_api_token = os.environ.get('VTEX_STORE_API_TOKEN')
+        # vtex_store = os.getenv('VTEX_STORE')
+        # vtex_store_api_key = os.environ.get('VTEX_STORE_API_KEY')
+        # vtex_store_api_token = os.environ.get('VTEX_STORE_API_TOKEN')
 
-        user_product_list = tracker.get_slot("product_list")
-        print(user_product_list)
+        entities = tracker.latest_message["entities"]
+        logger.info(f"Extracted entities: {pformat(entities)}")
+
+        message = "Lista de produtos: "
+        current_group = 1
+
+        for current_group in range(1, len(entities)):
+            logger.info(f"Loop {current_group}")
+
+            product = next(tracker.get_latest_entity_values(entity_type="product", entity_group=str(current_group)), None)
+            quantity = next(tracker.get_latest_entity_values(entity_type="quantity", entity_group=str(current_group)), None)
+
+            if product != None or quantity != None:
+                message += f"\n{product}: ({quantity})"
+
 
         # Get product list from VTEX store
         # match against user provided input
@@ -38,8 +59,5 @@ class ActionProductList(Action):
         # We need to give back a list of items that didn't quite match
         # the user intent and ask them to provide more information or just forget about it
 
-        try:
-            dispatcher.utter_message("A orderm do seu product é {}?".format(product_list))
-        except ValueError:
-            dispatcher.utter_message(ValueError)
-        return [SlotSet("telefone", telefone)]
+        dispatcher.utter_message(message)
+        return []
